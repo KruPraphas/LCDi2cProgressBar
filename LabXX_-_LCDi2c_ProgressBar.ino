981d80d289d86d2e0e29d014537ec35f81f6340d
Kru Praphas Suwanphet <praphas@hotmail.com>
2021-03-21 21:23:19 +0700

    Update LabXX_-_LCDi2c_ProgressBar.ino

--- LabXX_-_LCDi2c_ProgressBar.ino
+++ LabXX_-_LCDi2c_ProgressBar.ino
@@ -1,7 +1,5 @@
-// LCD i2c Library https://github.com/johnrickman/LiquidCrystal_I2C
-
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h>        //https://github.com/johnrickman/LiquidCrystal_I2C
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 byte zero[] = {
   B11111,
